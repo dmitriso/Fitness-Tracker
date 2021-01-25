fa7660e6bf14882f8c1eedcbaf0713f3563b094e
Dmitri So <dmitriso@yahoo.com>
2021-01-25 15:22:19 -0500

    Created HTML route for index.html page.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 module.exports = function(app) {
     //HTML ROUTE FOR "/"
-
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(__dirname, "./index.html"))
+    })
     //HTML ROUTE FOR "/EXERCISE"
+    
 
     //HTML ROUTE FOR "/STATS"
 }
