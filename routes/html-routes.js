943297adb973232b104ace69dde36e75382056ab
Dmitri So <dmitriso@yahoo.com>
2021-01-25 15:51:01 -0500

    Changing pathname for html routes.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,18 @@
 // HTML ROUTES
 const path = require("path");
 
+//EXPORTING MODULE
 module.exports = function(app) {
     //HTML ROUTE FOR "/"
     app.get("/", (req,res) => {
-        res.sendFile(path.join(__dirname, "./index.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     })
     //HTML ROUTE FOR "/EXERCISE"
     app.get("/exercise", (req,res) => {
-        res.sendFile(path.join(__dirname, "./exercise.html"))
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
     })
     //HTML ROUTE FOR "/STATS"
     app.get("/stats", (req,res) => {
-        res.sendFile(path.join(__dirname, "./stats.html"))
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
     })
 }
