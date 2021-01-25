5579879fc4e701e8ce069f623c09a3f40e456399
Dmitri So <dmitriso@yahoo.com>
2021-01-25 15:40:55 -0500

    Setting pseudo code/Creating a module to export.

---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+// API ROUTES
+const Workout = require("../models/workout.js")
+
+//EXPORTING MODULE
+module.exports = function(app) {
+//API ROUTE TO GET/FIND ALL WORKOUTS
+
+// API ROUTE TO GET/FIND 
+
+// API ROUTE TO POST/CREATE A NEW WORKOUT
+
+// API ROUTE TO PUT/UPDATE A WORKOUT
+}
