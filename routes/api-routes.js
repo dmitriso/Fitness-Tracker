0f49d4cfcff42a93f543ec1a8127f899f9cd2940
Dmitri So <dmitriso@yahoo.com>
2021-01-25 21:17:15 -0500

    Refactoring api route for putting/updating a workout.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,38 @@
 // API ROUTES
-const router = require("express").Router();
-const { Router } = require("express");
+const mongoose = require("mongoose");
 const Workout = require("../models/workout.js");
 
 
 //EXPORTING MODULE
-module.exports = function(app) {
-//API ROUTE TO GET/FIND ALL WORKOUTS("/api/workouts")
-app.get("/api/workouts", (req,res) => {
-    Workout.find({}, (err,data) => {
-        if(err) throw err;
-        res.json(data);
+module.exports = function (app) {
+    //API ROUTE TO GET/FIND ALL WORKOUTS("/api/workouts")
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({}, (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        });
     });
-});
-//API ROUTE TO GET/FIND 
 
-//API ROUTE TO POST/CREATE A NEW WORKOUT
-app.post("/api/workouts", ({ body },res) => {
-    Workout.insert(body, (err,data) => {
-        if(err) throw err;
-        res.json(data);
+    //API ROUTE TO GET/FIND 
+
+
+    //API ROUTE TO POST/CREATE A NEW WORKOUT
+    app.post("/api/workouts", ({ body }, res) => {
+        Workout.create({ body }, (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        });
     });
-});
-//API ROUTE TO PUT/UPDATE A WORKOUT
-app.put("/api/workouts/:id", ({ body },res) => {
-    Workout.update({"_id": mongojs.ObjectId(id)},{$set: { body }}, (err,data) => {
-        if(err) throw err;
-        res.json(data);
-    })
-})
+
+    //API ROUTE TO PUT/UPDATE A WORKOUT
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.body);
+        console.log(req.params);
+        Workout.update({ id: req.params.id }, { $set: { "type": req.body.type, "name": req.body.name, "distance": req.body.distance, "duration": req.body.duration } }, (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        });
+    });
+
+
 }
