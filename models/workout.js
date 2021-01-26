b5107ff0fc021d9e457919b08cd056efe515bacc
Dmitri So <dmitriso@yahoo.com>
2021-01-26 15:27:05 -0500

    Added some comments for readability.

--- models/workout.js
+++ models/workout.js
@@ -1,51 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
+//MODEL SCHEMA FOR WORKOUT COLLECTION AND EXERCISE OBJECTS
 const newWorkout = new Schema({
-    exercises: [{
+    exercise: [{
         type: {
             type: String,
-            trim: true,
             required: "Type of workout is required"
         },
-
         name: {
             type: String,
             trim: true,
             required: "The name is required!"
         },
-
         distance: {
             type: Number,
             required: "Distance is required!"
         },
-
         duration: {
             type: Number,
             required: "Duration is required!"
         },
-
         weight: {
             type: Number,
             required: "The weight is required!"
         },
-
         sets: {
             type: Number,
             required: "The number sets is required!"
-
         },
-
         reps: {
             type: Number,
             required: "The number of reps is required!"
         },
     }]
-
 });
 
 const Workout = mongoose.model("Workout", newWorkout);
-
+//EXPORTING MODULE
 module.exports = Workout;
 
