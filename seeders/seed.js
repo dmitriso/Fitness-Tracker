a0c4a6fdd884975a6da3ff97a8164027cbfa2c43
Dmitri So <dmitriso@yahoo.com>
2021-01-26 22:44:36 -0500

    Deploying to Heroku.

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,7 +3,10 @@ let Workout = require("../models/workout.js");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
 });
 
 let workoutSeed = [
--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(morgan("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
 });
 
 //ROUTES
